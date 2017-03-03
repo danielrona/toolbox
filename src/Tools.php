64ed38579b6a6e4634ce7bf151f6b492c1c0c987
Daniel <danielrona@users.noreply.github.com>
2017-03-03 15:50:05 +0100

    Update Tools.php
    added ean validation

--- src/Tools.php
+++ src/Tools.php
@@ -20,5 +20,23 @@
         {
             return base64_encode(openssl_random_pseudo_bytes($length));
         }
-
+        
+        function validateEAN($code)
+        {
+            if (strlen($code) <> 12) {
+                return 'Input needs to be exactly 12 chars';
+            } else {
+                $key = 0;
+                $mult = array(1, 3);
+                for ($i = 0; $i < strlen($code); $i++) {
+                    $key += substr($code, $i, 1) * $mult[$i % 2];
+                }
+                $key = 10 - ($key % 10);
+                if ($key == 10) {
+                    $key = 0;
+                }
+                $code .= $key;
+            }
+        return $code;
+        }
     }
