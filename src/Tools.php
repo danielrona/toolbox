3b7b5e42095c87f03ce2ad860b1848471a4e36ca
Daniel 'ScHaTtEn' Rona <git@go-mail.me>
2017-10-21 21:05:53 +0200

    added dateDifference function

--- src/Tools.php
+++ src/Tools.php
@@ -20,14 +20,14 @@
         {
             return base64_encode(openssl_random_pseudo_bytes($length));
         }
-        
+
         function validateEAN($code)
         {
             if (strlen($code) <> 12) {
                 return 'Input needs to be exactly 12 chars';
             } else {
                 $key = 0;
-                $mult = array(1, 3);
+                $mult = array (1, 3);
                 for ($i = 0; $i < strlen($code); $i++) {
                     $key += substr($code, $i, 1) * $mult[$i % 2];
                 }
@@ -37,6 +37,18 @@
                 }
                 $code .= $key;
             }
-        return $code;
+
+            return $code;
+        }
+
+        function dateDifference($date_1, $date_2, $differenceFormat = '%a')
+        {
+            $datetime1 = date_create($date_1);
+            $datetime2 = date_create($date_2);
+
+            $interval = date_diff($datetime1, $datetime2);
+
+            return $interval->format($differenceFormat);
+
         }
     }
