ccb8d8961e48d7ca0558b528798f3a6d808e80f3
Daniel 'ScHaTtEn' Rona <git@go-mail.me>
2018-02-17 10:06:38 +0100

    moved to subdirectory

---
+++ src/danielrona/Config.php
@@ -0,0 +1,59 @@
+<?php
+    /**
+     * Copyright (c) 2017-2018.
+     *
+     * Daniel Rona
+     */
+
+    namespace Danielrona\Toolbox;
+
+
+    /**
+     * Class Config
+     * @package Danielrona\Toolbox
+     */
+    class Config
+    {
+        static $confArray;
+        private static $instance;
+
+        /**
+         * @return $this
+         */
+        public static function getInstance()
+        {
+            if (!isset(self::$instance)) {
+                $object = __CLASS__;
+                self::$instance = new $object;
+            }
+
+            return self::$instance;
+        }
+
+        /**
+         * @param $name
+         *
+         * @return string
+         */
+        public static function get($name)
+        {
+            return self::$confArray[$name];
+        }
+
+        /**
+         * @param $name
+         * @param $value
+         */
+        public static function set($name, $value)
+        {
+            self::$confArray[$name] = $value;
+        }
+
+        /**
+         * @return bool
+         */
+        function isWorking()
+        {
+            return true;
+        }
+    }
